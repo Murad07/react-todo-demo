ed141456300aa0c4340cfd1faf9984cbe7a4acbd
Murad Hossain <sliverboymurad@gmail.com>
2020-05-31 13:58:59 +0600

    Updates

---
+++ precache-manifest.5a351dd5220911e4f7b036ac90e359d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad34673ad48e0391b290aa2ffbe3ff7",
+    "url": "/react-todo-demo/index.html"
+  },
+  {
+    "revision": "53b51e20c8f308d0f230",
+    "url": "/react-todo-demo/static/css/main.e231f84d.chunk.css"
+  },
+  {
+    "revision": "5e3b9d4f5c49ce162982",
+    "url": "/react-todo-demo/static/js/2.0467a4f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-demo/static/js/2.0467a4f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b51e20c8f308d0f230",
+    "url": "/react-todo-demo/static/js/main.44c9018e.chunk.js"
+  },
+  {
+    "revision": "1ca812ac4bdae662db9d",
+    "url": "/react-todo-demo/static/js/runtime-main.892bf2a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
